7773e16f9fc2930c08a02cd3764473212f3e56cc
Nicolas Truessel <nicolas+git@ntruessel.ch>
2020-05-21 18:17:54 +0200

    Setup Jooq, Hikari & Liquibase with Micronaut

--- server/src/main/kotlin/ch/ntruessel/broccoli/server/infrastructure/rest/RecipeController.kt
+++ server/src/main/kotlin/ch/ntruessel/broccoli/server/infrastructure/rest/RecipeController.kt
@@ -2,11 +2,19 @@ package ch.ntruessel.broccoli.server.infrastructure.rest
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import java.util.*
 
 @Controller("/recipes")
 class RecipeController {
 
     @Get
-    fun demo() {}
+    fun recipes() {}
+
+    @Get("/{id}")
+    fun recipe(id: UUID) {}
+
+    @Post()
+    fun create() {}
 
 }
