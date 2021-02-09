9d658bb5ec0069679ae2fe777246a1e9c01a6b41
Nicolas Truessel <nicolas+git@ntruessel.ch>
2021-02-09 21:56:37 +0100

    Minimal Material UI Setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppBar, Container, Toolbar } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return <>
+        <Router>
+            <AppBar position="fixed">
+                <Toolbar/>
+            </AppBar>
+            <Container>
+            </Container>
+        </Router>
+    </>;
 }
 
 export default App;
