95eb3a20f22616d54f4e53357625c28a31206d9e
Nicolas Trüssel <nicolas+git@ntruessel.ch>
2021-04-25 15:42:18 +0200

    Reset to Bare Bones Frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AppBar, Container, styled, Toolbar } from '@material-ui/core';
 import { RecipeOverview } from './pages/RecipeOverview';
 import { CreateRecipe } from './pages/CreateRecipe';
 import { routes } from './utils/routes';
@@ -8,19 +7,14 @@ import { routes } from './utils/routes';
 function App() {
     return <>
         <Router>
-            <AppBar position="sticky">
-                <Toolbar/>
-            </AppBar>
-            <Container>
-                <Switch>
-                    <Route path={routes.overview} exact>
-                        <RecipeOverview/>
-                    </Route>
-                    <Route path={routes.newRecipe}>
-                        <CreateRecipe/>
-                    </Route>
-                </Switch>
-            </Container>
+            <Switch>
+                <Route path={routes.overview} exact>
+                    <RecipeOverview/>
+                </Route>
+                <Route path={routes.newRecipe}>
+                    <CreateRecipe/>
+                </Route>
+            </Switch>
         </Router>
     </>;
 }
--- web/src/pages/CreateRecipe.tsx
+++ web/src/pages/CreateRecipe.tsx
@@ -1,11 +1,6 @@
-import { TextField, Typography } from '@material-ui/core';
 import React from 'react';
 
 export function CreateRecipe() {
     return <>
-        <Typography variant="h4">Neues Rezept erfassen</Typography>
-        <form>
-            <TextField name="name" label="Name"/>
-        </form>
     </>;
 }
